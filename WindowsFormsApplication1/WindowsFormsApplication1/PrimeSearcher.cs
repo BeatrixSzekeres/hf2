f5b2ba724f5b9970922debeb1f56efd39b479af9
noemi rozsa <noemir1@outlook.hu>
2017-02-20 22:20:07 +0100

    prímszám keresés alap hozzáadva a PrimeSearcher-hez

--- WindowsFormsApplication1/WindowsFormsApplication1/PrimeSearcher.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/PrimeSearcher.cs
@@ -8,5 +8,24 @@ namespace WindowsFormsApplication1
 {
     class PrimeSearcher
     {
+        static PrimeSearcher()
+        {
+            int pixel = 2; // ha a ciklus a form pixelein megy végig, ez a változó lehetne az index. A kezdőérték csak ideiglenes!!
+            int oszto = 0;
+            for (int i = 1; i <= pixel; i++)
+            {
+                if (pixel % i == 0)
+                    oszto++;
+                else { }
+            }
+            if (oszto == 2)
+            {
+                //a szám prím, kifestés
+            }
+            else
+            {
+                //nem prím, nincs kifestés
+            }
+        }
     }
 }
